9f5613417ac9c03eb33b6118ee45b7e307cf4238
mfadhili <mfadhili@github.com>
2023-01-30 08:09:47 +0300

    cards html

--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
  import { Injectable } from '@angular/core';
+ import {Food} from "../../shared/food/food";
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,75 @@ export class FoodService {
 
   constructor() { }
 
-  getAll():String[]{
+  getAll():Food[]{
     return [
-      '/assets/images/foods/food-1.jpg',
-      '/assets/images/foods/food-2.jpg',
-      '/assets/images/foods/food-3.jpg',
-      '/assets/images/foods/food-4.jpg',
-      '/assets/images/foods/food-5.jpg',
-      '/assets/images/foods/food-6.jpg'
+      {
+        id:1,
+        name: "Pizza Pepperoni",
+        cooktime: '10-20',
+        favorite: true,
+        origins: ['italy'],
+        stars: 5,
+        imageUrl: '/assets/images/foods/food-1.jpg',
+        price: 10
+
+      },
+      {
+        id:2,
+        name: "Meatball",
+        cooktime: '20-30',
+        favorite: true,
+        origins: ['persia', 'middle east', 'china'],
+        stars: 4.7,
+        imageUrl: '/assets/images/foods/food-2.jpg',
+        price:  20,
+        tags: ['SlowFood', 'Lunch']
+
+      },
+      {
+        id: 3,
+        name: 'Hamburger',
+        price: 5,
+        cooktime: '10-15',
+        favorite: false,
+        origins: ['germany', 'us'],
+        stars: 3.5,
+        imageUrl: '/assets/images/foods/food-3.jpg',
+        tags: ['FastFood', 'Hamburger'],
+      },
+      {
+        id: 4,
+        name: 'Fried Potatoes',
+        price: 2,
+        cooktime: '15-20',
+        favorite: true,
+        origins: ['belgium', 'france'],
+        stars: 3.3,
+        imageUrl: '/assets/images/foods/food-4.jpg',
+        tags: ['FastFood', 'Fry'],
+      },
+      {
+        id: 5,
+        name: 'Chicken Soup',
+        price: 11,
+        cooktime: '40-50',
+        favorite: false,
+        origins: ['india', 'asia'],
+        stars: 3.0,
+        imageUrl: '/assets/images/foods/food-5.jpg',
+        tags: ['SlowFood', 'Soup'],
+      },
+      {
+        id: 6,
+        name: 'Vegetables Pizza',
+        price: 9,
+        cooktime: '40-50',
+        favorite: false,
+        origins: ['italy'],
+        stars: 4.0,
+        imageUrl: '/assets/images/foods/food-6.jpg',
+        tags: ['FastFood', 'Pizza', 'Lunch'],
+      },
     ]
   }
 }
